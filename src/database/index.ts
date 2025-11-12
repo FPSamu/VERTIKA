d8437b586e99f6b30343e5db24386e25888b3fa5
Alan Varela <alanvareli@gmail.com>
2025-11-12 17:29:44 -0600

    conexion a vertika mongo

--- src/database/index.ts
+++ src/database/index.ts
@@ -2,8 +2,10 @@ import { connect } from "mongoose";
 
 const dbConnect = async (): Promise<void> => {
   try {
-    await connect(process.env.MONGO_URL!);
-    console.log("MongoDB conectado correctamente");
+    await connect(process.env.MONGO_URL!, {
+      dbName: process.env.DB_NAME,
+    });
+    console.log(`MongoDB conectado a la base de datos: ${process.env.DB_NAME}`);
   } catch (err) {
     console.error("Error al conectar con MongoDB:", err);
     throw err;
